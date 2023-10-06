31818385d9ad49371a9e10ff47b6ab3b63febb5c
Jahz10 <bullymedont.justify@gmail.com>
2023-10-06 17:16:53 +0800

    changed file location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from 'next/image'
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+import { BsFillDice5Fill } from "react-icons/bs";
+import useSWR from "swr";
 
 export default function Home() {
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Worrld</h1>
+    <main className="flex h-full w-full min-h-screen flex-col bg-gray-800 items-center justify-center">
+      {/* Advise Card  */}
+      <div className="relative w-[360px] h-[350px] bg-gray-700 space-y-8 py-8 rounded-xl px-6 text-center">
+        <p className="text-sm text-green-300 tracking-widest font-semibold">
+          ADVISE # 117
+        </p>
+        <h1 className="text-2xl text-gray-200 font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          recusandae dignissimos
+        </h1>
+
+        <div className="x w-full flex justify-between items-center gap-4">
+          {/* line 1 */}
+          <div className="w-full h-[1px] bg-gray-500" />
+          <div className="w-5 flex justify-center items-center gap-2">
+            <div className="w-[8px] h-4 rounded bg-gray-300" />
+            <div className="w-[8px] h-4 rounded bg-gray-300" />
+          </div>
+          {/* line 1 */}
+          <div className="w-full h-[1px] bg-gray-500" />
+          {/* Dice button  */}
+          <div className="absolute flex justify-center items-center bottom-[-65px] left-1/2 -translate-x-1/2 -translate-y-1/2  w-16 h-16 bg-green-400 hover:bg-green-500 rounded-full cursor-pointer">
+            <BsFillDice5Fill size={25} className="text-gray-800" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
